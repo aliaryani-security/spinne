1b338e15839dc18cc841535b4975b2429542cbe8
Ali Aryani <lenesis@proton.me>
2025-08-03 03:59:20 -0400

    finalize and bug fix

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import requests
 
-url = 'https://kernel.org/'
+url = 'https://wikipedia.org'
 urls_list = []
 is_followed = {}
 urls_list.append(url)
@@ -11,28 +11,45 @@ def check_url_list (url) -> bool:
         return True
     else:
         return False
+def check_follow (url) -> bool:
+    for entry in is_followed.keys():
+        if url != entry:
+            return False
+        else:
+            if is_followed[url]:
+                return True
+            else:
+                return False
 
-page = requests.get (url)
-is_followed[url] = True
 start = "http"
 end = ''
-for line in page.text.split('\n'):
-    if "http" in line:
-        if "kernel.org" in line:
-            if '">' in line:
-                end = '">'
-            else:
-                end = '" '
+try:
+    for URL in urls_list:
+        if check_follow(URL) != True:
             try:
-                sliced = line[line.index(start):line.index(end)]
-                if '"' in sliced:
-                    end = '"'
-                    parsed_url = sliced[sliced.index(start):sliced.index(end)]
-                else:
-                    parsed_url = sliced
-                if check_url_list(parsed_url) == False:
-                    urls_list.append (parsed_url)
+                page = requests.get (URL)
             except:
                 continue
-
-print (urls_list)
+            is_followed[URL] = True
+        for line in page.text.split('\n'):
+            if "http" in line:
+                if "wikipedia.org" in line:
+                    if '">' in line:
+                        end = '">'
+                    else:
+                        end = '" '
+                    try:
+                        sliced = line[line.index(start):line.index(end)]
+                        if '"' in sliced:
+                            end = '"'
+                            parsed_url = sliced[sliced.index(start):sliced.index(end)]
+                        else:
+                            parsed_url = sliced
+                        if check_url_list(parsed_url) == False:
+                            urls_list.append (parsed_url)
+                            is_followed[parsed_url] = False
+                    except:
+                        continue
+finally:
+    for url in urls_list:
+        print (url)
