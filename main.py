965011d6ca0992af4320a0d11e28245f17fb8b76
Ali Aryani <lenesis@proton.me>
2025-08-03 01:52:56 -0400

    add followed check

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import requests
 
 url = 'http://192.168.1.101'
 urls_list = []
+is_followed = {}
 urls_list.append(url)
 
 page = requests.get (url)
-print (page.text)
+is_followed[url] = True
+print (is_followed)
