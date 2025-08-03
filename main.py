cf88b6e91a39d93d227e0fb821180aa28e532bdf
Ali Aryani <lenesis@proton.me>
2025-08-03 03:28:13 -0400

    slice urls

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 import requests
 
-url = 'http://google.com'
+url = 'https://kernel.org/'
 urls_list = []
 is_followed = {}
 urls_list.append(url)
 
 page = requests.get (url)
 is_followed[url] = True
-
+start = "http"
+end = ''
 for line in page.text.split('\n'):
     if "http" in line:
-        if "google.com" in line:
-            print (line)
+        if "kernel.org" in line:
+            if '">' in line:
+                end = '">'
+            else:
+                end = '" '
+            try:
+                sliced = line[line.index(start):line.index(end)]
+                if '"' in sliced:
+                    end = '"'
+                    print (sliced[sliced.index(start):sliced.index(end)])
+                else:
+                    print (sliced)
+            except:
+                continue
