6a3ddcff743a486ed1462888695718dcdb881e61
Ali Aryani <lenesis@proton.me>
2025-08-03 03:33:23 -0400

    check url and append

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ urls_list = []
 is_followed = {}
 urls_list.append(url)
 
+def check_url_list (url) -> bool:
+    if url in urls_list:
+        return True
+    else:
+        return False
+
 page = requests.get (url)
 is_followed[url] = True
 start = "http"
@@ -21,8 +27,12 @@ for line in page.text.split('\n'):
                 sliced = line[line.index(start):line.index(end)]
                 if '"' in sliced:
                     end = '"'
-                    print (sliced[sliced.index(start):sliced.index(end)])
+                    parsed_url = sliced[sliced.index(start):sliced.index(end)]
                 else:
-                    print (sliced)
+                    parsed_url = sliced
+                if check_url_list(parsed_url) == False:
+                    urls_list.append (parsed_url)
             except:
                 continue
+
+print (urls_list)
